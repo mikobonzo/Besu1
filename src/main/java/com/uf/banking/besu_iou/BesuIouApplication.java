1bec83f59d2e3219492ed30525470f701cc5bdd0
michael-mayor <michael.mayor@uberfusion.com>
2026-09-21 11:26:06 +0800

    Initials
    Initials

---
+++ src/main/java/com/uf/banking/besu_iou/BesuIouApplication.java
@@ -0,0 +1,13 @@
+package com.uf.banking.besu_iou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BesuIouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BesuIouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uf/banking/besu_iou/BesuIouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uf.banking.besu_iou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BesuIouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
